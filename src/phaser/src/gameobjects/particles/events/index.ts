09182d3ec8e168d009fdf9986f3cf3313bb43164
Francisco Pereira Alvarado <gammafp@gmail.com>
2026-02-02 18:03:05 +0100

    more events

---
+++ src/phaser/src/gameobjects/particles/events/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @author       Richard Davey <rich@phaser.io>
+ * @copyright    2013-2025 Phaser Studio Inc.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * @namespace Phaser.GameObjects.Particles.Events
+ */
+
+export { COMPLETE } from './COMPLETE_EVENT';
+export { DEATH_ZONE } from './DEATH_ZONE_EVENT';
+export { EXPLODE } from './EXPLODE_EVENT';
+export { START } from './START_EVENT';
+export { STOP } from './STOP_EVENT';
---
+++ src/phaser/src/renderer/webgl/pipelines/events/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @author       Richard Davey <rich@phaser.io>
+ * @copyright    2013-2025 Phaser Studio Inc.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * @namespace Phaser.Renderer.WebGL.Pipelines.Events
+ */
+
+export { AFTER_FLUSH } from './AFTER_FLUSH_EVENT';
+export { BEFORE_FLUSH } from './BEFORE_FLUSH_EVENT';
+export { BIND } from './BIND_EVENT';
+export { BOOT } from './BOOT_EVENT';
+export { DESTROY } from './DESTROY_EVENT';
+export { REBIND } from './REBIND_EVENT';
+export { RESIZE } from './RESIZE_EVENT';
