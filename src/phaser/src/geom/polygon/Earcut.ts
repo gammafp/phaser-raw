0ada8a2068909f15bdb2ff9b9ff2ff62fe0cd9b9
Francisco Pereira Alvarado <gammafp@gmail.com>
2026-02-16 19:31:10 +0100

    Update Earcut.ts

--- src/phaser/src/geom/polygon/Earcut.ts
+++ src/phaser/src/geom/polygon/Earcut.ts
@@ -3,7 +3,6 @@
  * @copyright    2013-2025 Phaser Studio Inc.
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
-// TODO: Convert this complex algorithm to TypeScript
 
 /**
  * This module implements a modified ear slicing algorithm, optimized by z-order curve hashing and extended to
